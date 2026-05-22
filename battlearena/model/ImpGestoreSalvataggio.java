b39eefa2e2e35ef043ca9991bfa828b662e3bfa5
s_vnrlcu08d01l500e <s_vnrlcu08d01l500e@ltsp-current.itis-emattei.local>
2026-05-22 13:05:06 +0200

    creazione dir salvataggi

---
+++ battlearena/model/ImpGestoreSalvataggio.java
@@ -0,0 +1,17 @@
+package battlearena.model;
+import java.io.File;
+
+public class ImpGestoreSalvataggio implements GestoreSalvataggio{
+
+    //File azioni = new File()
+
+    @Override
+    public void registraAzioneLog(String azione) throws Exception {
+
+    }
+
+    @Override
+    public void salvaVincitore(String nomePersonaggio) throws Exception {
+
+    }
+}
